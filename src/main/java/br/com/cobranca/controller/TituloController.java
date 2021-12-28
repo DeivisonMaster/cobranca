5e1983ed8942d39c68fb4712d608aea46ac71e59
Deivison Oliveira <deivisonoliveira.info@gmail.com>
2021-12-28 16:10:16 -0400

    implementando a edição de titulo

--- src/main/java/br/com/cobranca/controller/TituloController.java
+++ src/main/java/br/com/cobranca/controller/TituloController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -21,19 +22,20 @@ import br.com.cobranca.repository.TituloRepository;
 @RequestMapping("/titulos")
 public class TituloController {
 	
+	private static final String CADASTRO_VIEW = "CadastroTitulo";
 	@Autowired
 	private TituloRepository repository;
 	
 	@RequestMapping("/novo")
 	public ModelAndView novo(){
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
 		mv.addObject("titulo", new Titulo());
 		return mv;
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
 	public ModelAndView novo(@Validated Titulo titulo, Errors erros, RedirectAttributes redirect){
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
 		if(erros.hasErrors()){
 			return mv;
 		}
@@ -48,6 +50,14 @@ public class TituloController {
 		List<Titulo> listaTitulos = repository.findAll();
 		ModelAndView mv = new ModelAndView("ConsultaTitulo");
 		mv.addObject("listaTitulos", listaTitulos);	
+		return mv;
+	}
+	
+	@RequestMapping("{id}")
+	public ModelAndView edicao(@PathVariable Long id){
+		Titulo titulo = repository.getOne(id);
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+		mv.addObject(titulo);
 		return mv;
 	}
 	
