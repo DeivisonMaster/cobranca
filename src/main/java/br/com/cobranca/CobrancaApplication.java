7367e3ea370a320c752bc3841136b91c0c3631c8
Deivison Oliveira <deivisonoliveira.info@gmail.com>
2021-12-30 10:17:57 -0400

    ajustes de links dinamicos via thymeleaf para deploy no tomcat 8.5

--- src/main/java/br/com/cobranca/CobrancaApplication.java
+++ src/main/java/br/com/cobranca/CobrancaApplication.java
@@ -5,7 +5,10 @@ import java.util.Locale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
@@ -25,6 +28,17 @@ public class CobrancaApplication {
 	@Bean
 	public LayoutDialect layoutDialect(){
 		return new LayoutDialect();
+	}
+	
+	@Configuration
+	public static class MvcConfig extends WebMvcConfigurerAdapter{
+		private static final String BARRA_PATH = "/";
+		private static final String TITULOS = "/titulos";
+
+		@Override
+		public void addViewControllers(ViewControllerRegistry registry) {
+			registry.addRedirectViewController(BARRA_PATH, TITULOS);
+		}
 	}
 	
 }
